e8d75f8ce46ba80119fbe1c88e7ae80dd76773f0
Roni <ronisank21@hotmail.com>
2024-04-01 22:38:14 -0300

    Add exercises 2 and 3

--- ExercicioTrello/app.js
+++ ExercicioTrello/app.js
@@ -1,3 +1,5 @@
+// Exercicio trello - 1
+
 const express = require('express');
 const app = express();
 
@@ -6,3 +8,27 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
+
+//Exercicio trello - 2
+
+app.get('/sobre', (req, res) => {
+    res.send('Página sobre: Aplicativo em Node.js');
+    });
+
+app.get('/contato', (req, res) => {
+    res.send('Para entrar em contato, envie um email para: email@email.com');
+    });
+
+//Exercicio trello - 3
+
+const middleware = (req, res, next) => {
+    const horaAtual = new Date().toISOString();
+    console.log(`[${horaAtual}] A requisição foi feita para a rota: ${req.method} ${req.originalUrl} `);
+    next();
+    }
+
+//app.use(middleware);
+
+app.get('/hora', middleware,(req, res) => {
+   res.send("A hora atual é: " + new Date().getHours() + ":" + new Date().getMinutes());
+    });
